5ad1920f47a81afdf261231b3a66dcbdff128416
GK-Sastry <134768981+GK-Sastry@users.noreply.github.com>
2023-06-19 14:56:19 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+str1.sort();
+str2.sort();
+ 
+ if(str1.length!=str2.length) return false;
+ for(var i =0;i<str1.length;i++){
+   if(str1[i]==str2[i]) continue;
+   else return false
 }
+ return true;
 
 module.exports = isAnagram;
